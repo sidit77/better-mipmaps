bae16d05edcf2a7b3bb955e32c31e484a5de9a28
sidit77 <sidit77@gmail.com>
2024-10-26 16:19:13 +0200

    upgraded the sodium version

--- src/main/java/com/github/sidit77/bettermipmaps/compat/sodium/mixin/SodiumGameOptionPagesMixin.java
+++ src/main/java/com/github/sidit77/bettermipmaps/compat/sodium/mixin/SodiumGameOptionPagesMixin.java
@@ -3,11 +3,11 @@ package com.github.sidit77.bettermipmaps.compat.sodium.mixin;
 import com.github.sidit77.bettermipmaps.BetterMipmaps;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import me.jellysquid.mods.sodium.client.gui.SodiumGameOptionPages;
-import me.jellysquid.mods.sodium.client.gui.options.Option;
-import me.jellysquid.mods.sodium.client.gui.options.OptionImpl;
-import me.jellysquid.mods.sodium.client.gui.options.control.ControlValueFormatter;
-import me.jellysquid.mods.sodium.client.gui.options.control.SliderControl;
+import net.caffeinemc.mods.sodium.client.gui.SodiumGameOptionPages;
+import net.caffeinemc.mods.sodium.client.gui.options.Option;
+import net.caffeinemc.mods.sodium.client.gui.options.OptionImpl;
+import net.caffeinemc.mods.sodium.client.gui.options.control.ControlValueFormatter;
+import net.caffeinemc.mods.sodium.client.gui.options.control.SliderControl;
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.At;
 abstract class SodiumGameOptionPagesMixin {
 
     @SuppressWarnings("unchecked")
-    @WrapOperation(method = "quality", at = @At(value = "INVOKE", target = "Lme/jellysquid/mods/sodium/client/gui/options/OptionImpl$Builder;build()Lme/jellysquid/mods/sodium/client/gui/options/OptionImpl;"), remap = false)
+    @WrapOperation(method = "quality", at = @At(value = "INVOKE", target = "Lnet/caffeinemc/mods/sodium/client/gui/options/OptionImpl$Builder;build()Lnet/caffeinemc/mods/sodium/client/gui/options/OptionImpl;"), remap = false)
     private static <S, T> OptionImpl<S, T> increaseMipmapLevels(OptionImpl.Builder builder, Operation<OptionImpl<S, T>> factory){
         OptionImpl<S, T> option = factory.call(builder);
         if(!option.getName().getString().equals(Minecraft.getInstance().options.mipmapLevels().toString()))
