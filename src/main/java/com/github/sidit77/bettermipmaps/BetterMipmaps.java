2f5eeebb26f35a5a1c96d320de3e7434f3de0c18
sidit77 <sidit77@gmail.com>
2025-02-19 21:54:54 +0100

    updated to 1.21.4

--- src/main/java/com/github/sidit77/bettermipmaps/BetterMipmaps.java
+++ src/main/java/com/github/sidit77/bettermipmaps/BetterMipmaps.java
@@ -1,15 +1,16 @@
 package com.github.sidit77.bettermipmaps;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.inventory.InventoryMenu;
 
 import java.util.Collections;
 import java.util.Set;
 
 public class BetterMipmaps implements ModInitializer {
 
-    public static Set<ResourceLocation> UPSCALE_WHITELIST = Collections.singleton(InventoryMenu.BLOCK_ATLAS);
+    @SuppressWarnings("deprecation")
+    public static Set<ResourceLocation> UPSCALE_WHITELIST = Collections.singleton(TextureAtlas.LOCATION_BLOCKS);
     public static int MAX_MIPMAP_LEVEL = 10;
 
     @Override
